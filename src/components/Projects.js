a6eb91b87945d1002869f27f5d4b089e9ed2d12a
RobertAntonyan <rob.ant.2014.ra@gmail.com>
2023-10-10 21:51:35 +0400

    second

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+// import projImg1 from "../assets/img/project-img1.png";
+// import projImg2 from "../assets/img/project-img2.png";
+// import projImg3 from "../assets/img/project-img3.png";
 import projImg4 from "../assets/img/project-img4.png";
 import projImg5 from "../assets/img/project-img5.png";
 import projImg6 from "../assets/img/project-img6.png";
 import projImg7 from "../assets/img/project-img7.png";
 import projImg8 from "../assets/img/project-img8.png";
 import projImg9 from "../assets/img/project-img9.png";
+import projImg10 from "../assets/img/project-img10.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -49,6 +50,12 @@ function Projects() {
           description: "Design & Development",
           imgUrl: projImg9,
         },
+        {
+          title: "Cars",
+          description: "Design & Development",
+          imgUrl: projImg10,
+        },
+
       ];
   return (
     <section className="project" id="projects">
